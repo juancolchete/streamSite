73abdb15b57e78372e13cb037bd2f04e2856b45e
juancolchete <juanudkp@gmail.com>
2026-02-18 15:35:24 -0300

    feat: only fill RTMP server if is not filled

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,12 +10,16 @@ sed -i -e 's|push rtmp://YOUR_STREAM_URL_2;||g' nginx.conf
 touch nginx.conf
 rm nginx.conf
 cp nginx-sample.conf nginx.conf
-echo "Stream KEY 1"
-read STREAM_KEY_1
-sed -i -e 's|YOUR_STREAM_URL_1|$STREAM_KEY_1|g' nginx.conf
-echo "Stream KEY 2"
-read STREAM_KEY_2
-sed -i -e 's|YOUR_STREAM_URL_2|$STREAM_KEY_2|g' nginx.conf 
+if [[ ! -z "$RTMP_SERVER_1" ]]; then
+  echo "RTMP SERVER 1:"
+  read RTMP_SERVER_1
+fi
+sed -i -e 's|YOUR_STREAM_URL_1|$RTMP_SERVER_1|g' nginx.conf
+if [[ ! -z "$RTMP_SERVER_2" ]]; then
+  echo "RTMP SERVER 2:"
+  read RTMP_SERVER_2
+fi
+sed -i -e 's|YOUR_STREAM_URL_2|$RTMP_SERVER_2|g' nginx.conf 
 docker compose up -d
 echo "RTMP_URL=rtmp://$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nginx-rtmp)/live" >> .env
 echo "TARGET_SITE=https://coinmarketcap.com" >> .env
